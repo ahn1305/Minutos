63cb575a8029d85f7fae492fe740834e011d7d8b
Ashwin.B <ahnashwin1305@gmail.com>
2023-03-01 22:15:29 +0530

    Update settings.py

--- minutos/settings.py
+++ minutos/settings.py
@@ -37,9 +37,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ab7710850@gmail.com'
-EMAIL_HOST_PASSWORD = 'grfbsxqyymukecvg'
-DEFAULT_EMAIL_FROM = 'Minutos <ab7710850@gmail.com>'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_EMAIL_FROM = 'Minutos <>'
 
 WEBSITE_URL = 'http://127.0.0.1:8000'
 ACCEPTATION_URL = WEBSITE_URL + '/signup/'
